b09324da4677a7be01ac46beb399cb2c10ba19f9
ilywisdom <59305526+ilywisdom@users.noreply.github.com>
2024-12-19 09:58:11 -0600

    Optimizing Code a lil

--- lua/weapons/weapon_hideface.lua
+++ lua/weapons/weapon_hideface.lua
@@ -37,6 +37,15 @@ end
 if CLIENT then
     local blackMaterial = Material("vgui/black")
     local screenW, screenH = ScrW(), ScrH()
+    local WEAPON_CLASS = "weapon_hideface"
+    local HEAD_BONE = "ValveBiped.Bip01_Head1"
+    local SQUARE_SIZE = {w = 20, h = 30}
+    local SQUARE_OFFSET = Vector(0, 0, 2)
+    
+    -- Cache math functions
+    local math_rad = math.rad
+    local math_cos = math.cos
+    local math_sin = math.sin
     
     -- Update screen resolution when it changes
     hook.Add("OnScreenSizeChanged", "HideFaceUpdateRes", function()
@@ -44,25 +53,28 @@ if CLIENT then
     end)
     
     hook.Add("PostDrawTranslucentRenderables", "HideFaceSquare", function()
-        -- Loop through all players instead of just LocalPlayer
+        surface.SetMaterial(blackMaterial)
+        surface.SetDrawColor(0, 0, 0, 255)
+        
         for _, player in ipairs(player.GetAll()) do
             if not IsValid(player) or not player:Alive() then continue end
             
             local activeWeapon = player:GetActiveWeapon()
-            if not IsValid(activeWeapon) or activeWeapon:GetClass() != "weapon_hideface" then continue end
+            if not IsValid(activeWeapon) or activeWeapon:GetClass() != WEAPON_CLASS then continue end
             
-            local headBone = player:LookupBone("ValveBiped.Bip01_Head1")
+            local headBone = player:LookupBone(HEAD_BONE)
             if not headBone then continue end
             
-            local headPos, headAng = player:GetBonePosition(headBone)
+            local headPos = player:GetBonePosition(headBone)
             if not headPos then continue end
             
-            -- Draw black square from multiple angles
-            for i = 0, 359, 90 do
-                cam.Start3D2D(headPos + Vector(0, 0, 2), Angle(0, i, 90), 0.1)
-                    surface.SetMaterial(blackMaterial)
-                    surface.SetDrawColor(0, 0, 0, 255)
-                    surface.DrawTexturedRect(-10, -15, 20, 30)
+            headPos:Add(SQUARE_OFFSET)
+            
+            -- Draw only 2 squares instead of 4 (facing front and side)
+            for i = 0, 1 do
+                local ang = i * 90
+                cam.Start3D2D(headPos, Angle(0, ang, 90), 0.1)
+                    surface.DrawTexturedRect(-SQUARE_SIZE.w/2, -SQUARE_SIZE.h/2, SQUARE_SIZE.w, SQUARE_SIZE.h)
                 cam.End3D2D()
             end
         end
